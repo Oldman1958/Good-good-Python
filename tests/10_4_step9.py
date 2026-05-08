d738cef5a3f301fe891c9c1603746dd157fcb616
Oldman1958 <boris_i@bk.ru>
2026-05-08 18:52:04 +0500

    Решение задач

---
+++ tests/10_4_step9.py
@@ -0,0 +1,28 @@
+"""
+Подвиг 7.
+В функцию get_data() передается параметр value:
+
+def get_data(value):
+    match value:
+        # здесь продолжайте программу
+
+    return None
+
+Необходимо дописать оператор match/case со следующими шаблонами:
+
+если переменная value имеет тип int и больше нуля, то вернуть значение переменной value;
+если переменная value имеет тип float и находится в диапазоне [-100; 100], то вернуть значение переменной value;
+если переменная value имеет тип str, то просто вернуть ее значение.
+P. S. Вызывать функцию не нужно, только дописать шаблоны.
+"""
+
+def get_data(value):
+    match value:
+        case int() as command if value > 0:
+            return value
+        case float() as command if -100 <= value <= 100:
+            return value
+        case str() as command:
+            return value
+
+    return None
