1c2fc50fa9c0b9925fd4e15dc990212807fbec05
Oldman1958 <boris_i@bk.ru>
2026-05-11 20:39:09 +0500

    Решение задач

--- tests/10_6_step6.py
+++ tests/10_6_step6.py
@@ -28,6 +28,12 @@ def parse_json(data):
         # здесь прописывайте шаблон
         case {'access': access, 'data': [date, *_]} if isinstance(access, bool) and isinstance(data['data'], list):
             return access, date
+
+        case {'id': ids, 'data': [_, {'login': login}, _, _]}:
+                return ids, login
+
+    return None
+
 """
 Подробное объяснение добавленного шаблона:
 {'access': access, 'data': [date, *_]} — шаблон для сопоставления структуры словаря:
@@ -39,10 +45,5 @@ if isinstance(access, bool) — дополнительная проверка т
 isinstance(data['data'], list) — проверяем, что значение поля 'data' действительно является списком.
 return access, date — возвращаем найденные значения в виде кортежа (access, date).
 """
-
-        case {'id': ids, 'data': [_, {'login': login}, _, _]}:
-            return ids, login
-
-    return None
 
 json_data = {'id': 2, 'access': False, 'data': ['26.05.2023', {'login': '1234', 'email': 'xxx@mail.com'}, 2000, 56.4]}
